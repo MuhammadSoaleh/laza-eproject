eb6f10522ca2abb8bd6c8b0b905cdab2efd6d742
MuhammadSoaleh <m.soaleh63@gmail.com>
2024-08-21 17:30:51 +0500

    styling done

--- lib/register.dart
+++ lib/register.dart
@@ -2,6 +2,7 @@ import 'package:laza/my_button.dart';
 import 'package:laza/my_textfeild.dart';
 import 'package:flutter/material.dart';
 import 'package:laza/authservice.dart';
+
 class RegisterPage extends StatelessWidget {
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passController = TextEditingController();
@@ -53,10 +54,10 @@ class RegisterPage extends StatelessWidget {
       body: Stack(
         children: [
           Positioned.fill(
-            child:
-             Image.network("https://cdn.pixabay.com/photo/2016/11/07/10/14/laptop-1805315_640.jpg",
-            fit: BoxFit.cover,)
-            ,
+            child: Image.network(
+              "https://cdn.pixabay.com/photo/2016/11/07/10/14/laptop-1805315_640.jpg",
+              fit: BoxFit.cover,
+            ),
           ),
           Opacity(opacity: 0.5),
           Center(
@@ -65,67 +66,62 @@ class RegisterPage extends StatelessWidget {
               children: [
                 SizedBox(height: 20),
                 Image.asset(
-                  "assets//images/Logo.png",
+                  "assets/images/Logo.png",
                   height: 250,
                   width: 300,
                 ),
-                // Text(
-                //   "REGISTER",
-                //   style: TextStyle(
-                //     fontSize: 24,
-                //     fontWeight: FontWeight.bold,
-                //     color: Colors.white,
-                //     shadows: [
-                //       Shadow(
-                //         blurRadius: 10.0,
-                //         color: Colors.black.withOpacity(0.5),
-                //         offset: Offset(0, 2),
-                //       ),
-                //     ],
-                //   ),
-                // ),
+                SizedBox(height: 20),
                 Column(
                   children: [
-                     SizedBox(height: 20),
-                     //email textfield
-          
-          MyTextField(
-            hintText: "Enter Your Email",
-            obscuretext: false,
-            textController: emailController,
-          ),
-                     SizedBox(height: 20),
-                              //pass textfield
-           SizedBox(height: 20),
-          MyTextField(
-            hintText: "Enter Your Password",
-            obscuretext: true,
-            textController: passController,
-          ),
-             SizedBox(height: 20),
-          MyTextField(
-            hintText: "Conform Your Password",
-            obscuretext: true,
-            textController: ConfirmpassController,
-          ),
-                   MyButton(text: "Register", ontap: () => register(context)),
+                    // Email TextField
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: MyTextField(
+                        hintText: "Enter Your Email",
+                        obscuretext: false,
+                        textController: emailController,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // Password TextField
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: MyTextField(
+                        hintText: "Enter Your Password",
+                        obscuretext: true,
+                        textController: passController,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    // Confirm Password TextField
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+                      child: MyTextField(
+                        hintText: "Confirm Your Password",
+                        obscuretext: true,
+                        textController: ConfirmpassController,
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    MyButton(text: "Register", ontap: () => register(context)),
                     Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                "Already Registered ? ",
-                style: TextStyle(color: Colors.black),
-              ),
-              GestureDetector(
-                onTap: ontap,
-                child: Text(
-                  " Login Now ",
-                  style: TextStyle(
-                      color: Color(0xff9775FA), fontWeight: FontWeight.bold),
-                ),
-              )
-            ],
-          )
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Already Registered? ",
+                          style: TextStyle(color: Colors.black),
+                        ),
+                        GestureDetector(
+                          onTap: ontap,
+                          child: Text(
+                            " Login Now ",
+                            style: TextStyle(
+                                color: Color(0xff9775FA),
+                                fontWeight: FontWeight.bold),
+                          ),
+                        )
+                      ],
+                    )
                   ],
                 ),
               ],
